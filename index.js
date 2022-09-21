347e8d2f9a2eac27b07d62786c90ab572d263006
Dave Greber <davegreber@gmail.com>
2022-09-21 09:02:03 -0400

    commit tree d8c021bd34a0659c10553756d13e53910b985ddf

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork(stuff = "go to the office"){
+    return `This Monday, I will ${stuff}.`
+}
+
+
+// function wrapAdjective (special = "*"){
+//     return function (
+//         //string 
+//         "You are "
+//     )
+
+//     wrapAdjective("%")("a dedicated programmer");
+
+function wrapAdjective(arg="*") {
+    return function(adj="special") {
+        return `You are ${arg }${adj}${arg}!`;
+        }
+    }
